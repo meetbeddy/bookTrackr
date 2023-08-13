99d984c15b9beab16d831a306287e8122e4ff0d4
Obed Okpala <obed@compumetrics.com.ng>
2023-08-13 19:10:28 +0100

    create admin on app init

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import user from "../models/users.model.js";
+import bcrypt from "bcrypt";
 
-const connect_db = () => {
+const connect_db = async () => {
 	try {
 		mongoose
 			.connect(process.env.MONGO_URI, {
@@ -8,6 +10,24 @@ const connect_db = () => {
 				useUnifiedTopology: true,
 			})
 			.then(() => console.log("database ready to go"));
+		const admin = await user.findOne({ email: "admin@admin.com" });
+		if (!admin) {
+			try {
+				const hashedPassword = await bcrypt.hash("admin-secret", 12);
+				const adm = await user.create({
+					password: hashedPassword,
+					firstName: "John",
+					lastName: "Doe",
+					phone: "07064492675",
+					email: "admin@admin.com",
+					role: "admin",
+				});
+				console.log("Admin Created Succesfully", adm);
+			} catch (e) {
+				console.log(e.message);
+				console.log(e);
+			}
+		}
 	} catch (e) {
 		console.log("🚀 ~ file: db_connection.js ~ line 12 ~ connect ~ e", e);
 	}
